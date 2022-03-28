04e783313527e5f1442cb3f59591f1a9825e72eb
mzmero <m.zmiro@hotmail.com>
2022-03-28 12:41:15 +0300

    fix conflict with navbar

--- citizenClient/src/view/pages/profile/profile.tsx
+++ citizenClient/src/view/pages/profile/profile.tsx
@@ -41,74 +41,76 @@ function Profile() {
     gender = <FemaleIcon style={{ color: "#FF69B4" }}></FemaleIcon>
 
   return (
-    <div className="profile">
+    <div>
       <Navbar></Navbar>
-      <div className='profile__title'>الملف الشخصي</div>
-      <div className="profile__photo">
-        <img src="https://previews.123rf.com/images/metelsky/metelsky1809/metelsky180900233/109815470-man-avatar-profile-male-face-icon-vector-illustration-.jpg"></img>
-      </div>
-      {userRole == "anonymous" ? (<div className='profile__btn'><Button variant="contained" style={{ width: "13.6875rem", backgroundColor: "#35b0d2", marginBottom: "0.5rem" }} type="submit" onClick={() => { navigate('/signup') }}>Register</Button>
-        <Button variant="contained" style={{ backgroundColor: "#35b0d2", width: "13.6875rem", marginBottom: "0.5rem" }} type="submit" onClick={() => { navigate('/login') }}>Log in</Button></div>) :
-        (<div className="profile__user"><div className='profile__name'>{user.name}</div>
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "1rem" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              label="email"
-              disabled
-              id="email_text"
-              defaultValue={user.email}
-              size="small"
-              sx={{ marginBottom: "0.5rem" }}
-            />
-            <TextField
-              label="city"
-              disabled
-              id="city_text"
-              defaultValue={user.location}
-              size="small"
-            />
-          </Box>
-          <div className="profile__links">
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2bh-content"
-                id="panel2bh-header"
-              >
-                <Typography sx={{ width: '70%', flexShrink: 0 }}>
-                  تغيير كلمة المرور
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <form onSubmit={handleUpdatePass}>
-                  <div className="profile__main__links__password">
-                    <div className="profile__main__links__password__1">
-                      <div className="profile__main__links__password__1__current">
-                        <TextField required fullWidth name="currentPass" label="كلمة المرور الحالية" variant="standard" type="password" onChange={onChangeUpdatePass} autoComplete={"off"} />
+      <div className="profile">
+        <div className='profile__title'>الملف الشخصي</div>
+        <div className="profile__photo">
+          <img src="https://previews.123rf.com/images/metelsky/metelsky1809/metelsky180900233/109815470-man-avatar-profile-male-face-icon-vector-illustration-.jpg"></img>
+        </div>
+        {userRole == "anonymous" ? (<div className='profile__btn'><Button variant="contained" style={{ width: "13.6875rem", backgroundColor: "#35b0d2", marginBottom: "0.5rem" }} type="submit" onClick={() => { navigate('/signup') }}>Register</Button>
+          <Button variant="contained" style={{ backgroundColor: "#35b0d2", width: "13.6875rem", marginBottom: "0.5rem" }} type="submit" onClick={() => { navigate('/login') }}>Log in</Button></div>) :
+          (<div className="profile__user"><div className='profile__name'>{user.name}</div>
+            <Box
+              component="form"
+              sx={{
+                '& .MuiTextField-root': { display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "1rem" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                label="email"
+                disabled
+                id="email_text"
+                defaultValue={user.email}
+                size="small"
+                sx={{ marginBottom: "0.5rem" }}
+              />
+              <TextField
+                label="city"
+                disabled
+                id="city_text"
+                defaultValue={user.location}
+                size="small"
+              />
+            </Box>
+            <div className="profile__links">
+              <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2bh-content"
+                  id="panel2bh-header"
+                >
+                  <Typography sx={{ width: '70%', flexShrink: 0 }}>
+                    تغيير كلمة المرور
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <form onSubmit={handleUpdatePass}>
+                    <div className="profile__main__links__password">
+                      <div className="profile__main__links__password__1">
+                        <div className="profile__main__links__password__1__current">
+                          <TextField required fullWidth name="currentPass" label="كلمة المرور الحالية" variant="standard" type="password" onChange={onChangeUpdatePass} autoComplete={"off"} />
+                        </div>
+                        <div className="profile__main__links__password__1__new">
+                          <TextField required fullWidth name="newPass" label="كلمة المرور الجديدة" variant="standard" type="password" onChange={onChangeUpdatePass} autoComplete={"off"} />
+                        </div>
                       </div>
-                      <div className="profile__main__links__password__1__new">
-                        <TextField required fullWidth name="newPass" label="كلمة المرور الجديدة" variant="standard" type="password" onChange={onChangeUpdatePass} autoComplete={"off"} />
-                      </div>
+                      <Button variant="contained" fullWidth style={{ marginTop: '1rem' }} type="submit"> :تغيير كلمة المرور</Button>
                     </div>
-                    <Button variant="contained" fullWidth style={{ marginTop: '1rem' }} type="submit"> :تغيير كلمة المرور</Button>
-                  </div>
-                </form>
-              </AccordionDetails>
-            </Accordion>
-          </div></div>)}
-      <div className="profile__btn">
-        <Button fullWidth value="chat" style={{ backgroundColor: "#8CBB46", width: "13.6875rem", marginBottom: "0.5rem" }} variant="contained" onClick={() => navigate('/previousAccidents')} >حوادث سابقة</Button>
-        {userRole != "anonymous" ?
-          <Button fullWidth value="logout" style={{ backgroundColor: "#8CBB46", width: "13.6875rem" }} variant="contained" onClick={(e: any) => { e.preventDefault(); dispatch(logOutUser()) }} >الخروج</Button> :
-          (<></>)}
-      </div>
-    </div >
+                  </form>
+                </AccordionDetails>
+              </Accordion>
+            </div></div>)}
+        <div className="profile__btn">
+          <Button fullWidth value="chat" style={{ backgroundColor: "#8CBB46", width: "13.6875rem", marginBottom: "0.5rem" }} variant="contained" onClick={() => navigate('/previousAccidents')} >حوادث سابقة</Button>
+          {userRole != "anonymous" ?
+            <Button fullWidth value="logout" style={{ backgroundColor: "#8CBB46", width: "13.6875rem" }} variant="contained" onClick={(e: any) => { e.preventDefault(); dispatch(logOutUser()) }} >الخروج</Button> :
+            (<></>)}
+        </div>
+      </div >
+    </div>
   )
 }
 export default Profile
